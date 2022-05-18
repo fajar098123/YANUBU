e4430942b5003a20b21dc551bcb1d0459e7e7f99
Moch. Fahrony <72066155+fahrony03@users.noreply.github.com>
2022-05-18 23:57:31 +0700

    update galeri user

--- yanubu/resources/views/guest/fasilitas.blade.php
+++ yanubu/resources/views/guest/fasilitas.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container-fluid">
+
+
     <h1>Fasilitas</h1>
+
+</div>
 @endsection
--- yanubu/resources/views/guest/galeri.blade.php
+++ yanubu/resources/views/guest/galeri.blade.php
@@ -1,1 +1,86 @@
 @extends('layouts.app')
+@section('content')
+{{-- <div class="container-fluid"> --}}
+
+   
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+
+        <div class="tz-gallery">
+            <section id="title" class="d-flex align-items-center">
+                <div class="container">
+                    <div class="d-flex justify-content-center">
+                        <div class="title-section">
+                            <h1>Galeri</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+    <div class="container gallery-container">
+
+            <div class="row">
+    
+                <div class="col-sm-12 col-md-4">
+                    <a class="lightbox" href="{{asset('assets/img/bridge.jpg')}}">
+                        <img src="{{asset('assets/img/bridge.jpg')}}" alt="Bridge">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="{{asset('assets/img/park.jpg')}}">
+                        <img src="{{asset('assets/img/park.jpg')}}" alt="Park">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="{{asset('assets/img/tunnel.jpg')}}">
+                        <img src="{{asset('assets/img/tunnel.jpg')}}" alt="Tunnel">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="{{asset('assets/img/traffic.jpg')}}">
+                        <img src="{{asset('assets/img/traffic.jpg')}}" alt="Traffic">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="{{asset('assets/img/traffic.jpg')}}">
+                        <img src="{{asset('assets/img/traffic.jpg')}}" alt="Coast">
+                    </a>
+                </div> 
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="{{asset('assets/img/traffic.jpg')}}">
+                        <img src="{{asset('assets/img/traffic.jpg')}}" alt="Rails">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="{{asset('assets/img/traffic.jpg')}}">
+                        <img src="{{asset('assets/img/traffic.jpg')}}" alt="Traffic">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="{{asset('assets/img/traffic.jpg')}}">
+                        <img src="{{asset('assets/img/traffic.jpg')}}" alt="Coast">
+                    </a>
+                </div> 
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="{{asset('assets/img/traffic.jpg')}}">
+                        <img src="{{asset('assets/img/traffic.jpg')}}" alt="Rails">
+                    </a>
+                </div>
+    
+            </div>
+    
+        </div>
+    
+    </div>
+    
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+    <script>
+        baguetteBox.run('.tz-gallery');
+    </script> --}}
+
+    
+
+
+{{-- </div> --}}
+@endsection
